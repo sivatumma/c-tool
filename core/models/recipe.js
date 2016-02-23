005c0619e32d6b6781b320b7e8cc2076641e79be
unknown <kranthichowdary.m@RMKLT079.CHSPLDC.COM>
2016-02-23 17:48:21 +0530

    msg

---
+++ core/models/recipe.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+
+var recipeSchema = new Schema({
+  
+	name: String,
+	cuisine: String,
+	subCusine: String,
+	recipeType: String,
+    	},
+	ingredients: [{
+	name: String,
+	qty: Number
+	uom: String
+ 		}],
+	});
+
+var Recipe = mongoose.model('Recipe', recipeSchema);
+module.exports = Recipe;
