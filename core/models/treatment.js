7b93da41408bff8d93752cdc19082396687cb6d4
unknown <kranthichowdary.m@RMKLT079.CHSPLDC.COM>
2016-02-26 15:52:49 +0530

    msg

---
+++ core/models/treatment.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+
+var treatmentSchema = new Schema({
+name: String,
+age: Number,
+weight: Number,
+overallHealth: String,
+medicalHistory: String,
+medications: Boolean,
+
+deceaseName: String,
+levels :[{
+          level1: Number,
+          level2: Number,
+        ]}
+treatment:[{
+          medications: String,
+          nutrition: String,
+          physicalActivity: String,
+          }]
+
+challenges:[{
+             weightloss: Number,
+             foodDiet: String,
+            }]
+reconsulting: Boolean,
+
+var Treatment = mongoose.model('Recipe', treatmentSchema);
+module.exports = Treatment;
+}]
+)};
